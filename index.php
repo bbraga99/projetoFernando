73d9f448311ceebb25bc98f89d84dab20f53359c
Bruno Braga <brunuu.henrique@hotmail.com>
2022-11-29 19:35:13 -0300

    Atualização

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$titulo = "Index";
+include "./cabecalho.php";
+?>
+
+<h1>
+    Simulados online<br>
+    <h2>Escolha entre cadastrar uma questão ou fazer algum simulado</h2>
+</h1>
+
+<?php
+include "./rodape.php";
+
+?>
---
+++ responder.php
@@ -0,0 +1,40 @@
+<?php
+$titulo = "Responder";
+include "./conexao.php";
+include "./cabecalho.php";
+$query = "select * from questoes order by rand() limit 15";
+$resultado = mysqli_query($conexao, $query);
+
+?>
+
+    <form action="./responder.php" method=""></form>
+
+<?php
+
+while ($linha = mysqli_fetch_array($resultado)) {
+?>  
+        <div class="card w-75">
+        <div class="card-body">
+        <h2 class="card-title"><?php echo $linha["pergunta"]; ?></h2>
+        <p class="card-text">
+            <input type="radio" name="<?php echo $linha["id"];?>" id="a" value="a" required> <?php echo $linha["a"]; ?><br>
+            <input type="radio" name="<?php echo $linha["id"];?>" id="b" value="b" required> <?php echo $linha["b"]; ?><br>
+            <input type="radio" name="<?php echo $linha["id"];?>" id="c" value="c" required> <?php echo $linha["c"]; ?><br>
+            <input type="radio" name="<?php echo $linha["id"];?>" id="d" value="d" required> <?php echo $linha["d"]; ?><br>
+            <input type="radio" name="<?php echo $linha["id"];?>" id="e" value="e" required> <?php echo $linha["e"]; ?><br>
+        </p>
+        <a href="#" class="btn btn-dark">Enviar resposta</a>
+    </div>
+    </div>
+<?php
+}
+
+
+?>
+
+    <button class="enviar btn btn-dark">ENVIAR PARA CORREÇÃO</button>
+
+
+<?php
+include "./rodape.php";
+?>
